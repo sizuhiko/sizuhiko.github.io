e5a37fee6ce601018ad0efbe2126a9dacf07b912
sizuhiko <k-kishida@esm.co.jp>
2023-04-05 17:47:13 +0900

    Automated commit at 2023-04-05 08:47:11 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/image-resize-ie.js
@@ -0,0 +1,1 @@
+var spans=document.getElementById("main").getElementsByTagName("span");for(var i=0;i<spans.length;i++){if(spans[i].className=="imgright"||spans[i].className=="imgleft"){var imgs=spans[i].getElementsByTagName("img");for(var j=0;j<imgs.length;j++){if(imgs[j].width>260){imgs[j].width="260";}}}}
